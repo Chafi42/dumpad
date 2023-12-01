eef5769fe034a746bddc1417fe51b5803dbd2c9a
Your Name <chafiaadssi42@gmail.com>
2023-12-01 14:39:41 +0100

    pad

--- tp.js
+++ tp.js
@@ -1,40 +1,45 @@
 let keys = document.querySelectorAll('.key');
 let audio = document.querySelectorAll('audio');
-
-console.log(keys);
-
-
-
-function playSound(){
-
-let caractere = ["A", "Z", "E", "Q", "S", "D", "W", "X", "C"]
-console.log(caractere);
-
-
-
-
-
-for (let i = 0; i < caractere.length; i++) {
-    
-        document.addEventListener('keypress', (son) => {
-if(son.key = caractere[i]) {
-audio[i].play();
+function playSound() {
+     let caractere = ["A", "Z", "E", "Q", "S", "D", "W", "X", "C"]
+        for (let i = 0; i < caractere.length; i++) {
+                document.addEventListener('keydown', (e) => {
+         if (e.key === caractere[i]) {
+                                keys[i].classList.add('playing');
+                                audio[i].play();
+                        }
+                        
+                        document.addEventListener('keyup', (e) => {
+                                if (e.key === caractere[i]) {
+                        keys[i].classList.remove('playing');
+                                }
+                        })
+                })
+        }
 }
-            
-
-            
-        });
+playSound()
 
 
-        
+// const touches = document.querySelectorAll('.key');
+// const musiques = document.querySelectorAll('audio');
+// let tlb = ['a', 'z', 'e', 'q', 's', 'd', 'w', 'x', 'c']
+// for (let i = 0; i < tlb.length; i++){
+//    document.addEventListener('keydown', (e) => {
+//       
+//       if (e.key === tlb[i]) {
+//          touches[i].classList.add('playing');
+//          musiques[i].play();
+//       }
 
-        
+//    })
 
+// document.addEventListener('keyup', (e) => {
+//   
 
+//    if (e.key === tlb[i]) {
 
+//       touches[i].classList.remove('playing');
+//    }
 
-    }
-}
-
-   playSound()
-    
+// })
+// }
